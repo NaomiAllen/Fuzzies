60a48a036bba15e3a5c689943da4a1a5aaeb08ed
Naomi <naomi.michelle.allen@gmail.com>
2020-10-15 09:15:24 -0500

    post route created

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 port= 3000
 
+app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res)=>{
     res.render('new.ejs');
     
 });
+
+app.post('/fuzzies/', (req,res)=>{
+    res.send(req.body)
+})
 
 
 
