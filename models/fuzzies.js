d1bddbf675996a9f6267d2688913903fac31f8eb
Naomi <naomi.michelle.allen@gmail.com>
2020-10-15 09:30:02 -0500

    model created

---
+++ models/fuzzies.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongooose');
+
+const fuzziesSchema = new mongoose.Schema({
+    name: {type: String, required :true},
+    location: {type: String, required: true},
+    Male: {type: checkbox, required: false},
+    Female: {type: checkbox, required: false},
+    description: {type: textarea, required: false}
+})
+
+const Fuzzies = mongoose.model('Fuzzies, fuzziesSchema');
+
+module.export = Fuzzies;
